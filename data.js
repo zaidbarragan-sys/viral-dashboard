755c11e90d121597cb3ebd236d425ef132cf20d0
Zaid Barragan <zaid@ip-192-168-0-194.ec2.internal>
2026-05-11 11:29:08 -0600

    Pre-viral: VIRAL_DEAL_PEPSICO_120326 — 3 SKUs, 39K MXN, 60% OFF, 15x

--- data.js
+++ data.js
@@ -1467,6 +1467,30 @@ const PREVIRAL_DEALS = [
     financiamiento_maker: "ADS",
     pct_growth_vs_maker: 30,
     categoria_display: "Cervezas"
+  },
+  {
+    deal_id: "VIRAL_DEAL_PEPSICO_120326",
+    maker_name: "PepsiCo",
+    maker_aux: "Pepsico",
+    business_unit: "Foods",
+    descuento_pct: 60,
+    incrementalidad: 15,
+    tipo_viral: "1 día",
+    ciudades: "CDMX",
+    fecha_planeacion: "2026-02-17",
+    fecha_ejecucion: "2026-03-12",
+    fecha_fin: null,
+    usd_mxn: 17.21,
+    fecha_tasa: "2026-02-17",
+    budget_maker_mxn: 30000,
+    budget_growth_mxn: 9000,
+    budget_total_mxn: 39000,
+    budget_maker_usd: 1743.17,
+    budget_growth_usd: 522.95,
+    budget_total_usd: 2266.13,
+    financiamiento_maker: "ADS",
+    pct_growth_vs_maker: 30,
+    categoria_display: "Galletas"
   }
 ];
 
@@ -1754,7 +1778,11 @@ const PREVIRAL_SKU_DETAIL = [
   { deal_id: "VIRAL_DEAL_HEINEKEN_300526", sync: "8524", ean: "7501049999278", pid: "2115776728", name: "Pack Cerveza Heineken Original Botella 6 x 355 mL", subcategoria: "Cervezas", h_units: 132.6, h_orders: 104.8, avg_price: 155.71, ex_incr_units: 663, ex_incr_orders: 524, ex_budget_mxn: 61941, ex_budget_usd: 3597.04, real_units: 445, final_budget_mxn: 41575, final_budget_usd: 2414.34, sku_order: 2 },
   { deal_id: "VIRAL_DEAL_HEINEKEN_300526", sync: "11434", ean: "7501061659365", pid: "2115621074", name: "Tecate Light Lata 12pack", subcategoria: "Cervezas", h_units: 85.4, h_orders: 75.7, avg_price: 206.02, ex_incr_units: 427, ex_incr_orders: 379, ex_budget_mxn: 52782, ex_budget_usd: 3065.16, real_units: 217, final_budget_mxn: 26824, final_budget_usd: 1557.72, sku_order: 3 },
   { deal_id: "VIRAL_DEAL_HEINEKEN_300526", sync: "10119", ean: "7501049912307", pid: "2114737060", name: "XX Dos Equis Lager Especial Lata 12pack 355 mL", subcategoria: "Cervezas", h_units: 67.9, h_orders: 55.4, avg_price: 237.96, ex_incr_units: 340, ex_incr_orders: 277, ex_budget_mxn: 48536, ex_budget_usd: 2818.58, real_units: 149, final_budget_mxn: 21274, final_budget_usd: 1235.43, sku_order: 4 },
-  { deal_id: "VIRAL_DEAL_HEINEKEN_300526", sync: "12099", ean: "7501061696995", pid: "2115748196", name: "Pack Cerveza Indio Tipo Vienna Oscura Lata 12 x 355 mL", subcategoria: "Cervezas", h_units: 64.0, h_orders: 56.9, avg_price: 212.14, ex_incr_units: 320, ex_incr_orders: 285, ex_budget_mxn: 40731, ex_budget_usd: 2365.33, real_units: 158, final_budget_mxn: 20110, final_budget_usd: 1167.83, sku_order: 5 }
+  { deal_id: "VIRAL_DEAL_HEINEKEN_300526", sync: "12099", ean: "7501061696995", pid: "2115748196", name: "Pack Cerveza Indio Tipo Vienna Oscura Lata 12 x 355 mL", subcategoria: "Cervezas", h_units: 64.0, h_orders: 56.9, avg_price: 212.14, ex_incr_units: 320, ex_incr_orders: 285, ex_budget_mxn: 40731, ex_budget_usd: 2365.33, real_units: 158, final_budget_mxn: 20110, final_budget_usd: 1167.83, sku_order: 5 },
+  // Pepsico
+  { deal_id: "VIRAL_DEAL_PEPSICO_120326", sync: "13979", ean: "7500478014569", pid: "2115411261", name: "Chokis Galletas Clásicas 76 g", subcategoria: "Galletas", h_units: 29.6, h_orders: 25.4, avg_price: 28.70, ex_incr_units: 444, ex_incr_orders: 381, ex_budget_mxn: 7647, ex_budget_usd: 444.34, real_units: 925, final_budget_mxn: 15929, final_budget_usd: 925.57, sku_order: 1 },
+  { deal_id: "VIRAL_DEAL_PEPSICO_120326", sync: "6221", ean: "7501000664221", pid: "2114047462", name: "Saladitas Galletas Clásicas 186 g", subcategoria: "Galletas", h_units: 23.7, h_orders: 17.8, avg_price: 28.57, ex_incr_units: 356, ex_incr_orders: 267, ex_budget_mxn: 6102, ex_budget_usd: 354.56, real_units: 745, final_budget_mxn: 12771, final_budget_usd: 742.07, sku_order: 2 },
+  { deal_id: "VIRAL_DEAL_PEPSICO_120326", sync: "80162", ean: "7500478013944", pid: "2115759953", name: "Chokis Rollo 180 g", subcategoria: "Galletas", h_units: 19.1, h_orders: 14.8, avg_price: 31.88, ex_incr_units: 287, ex_incr_orders: 222, ex_budget_mxn: 5490, ex_budget_usd: 319.00, real_units: 538, final_budget_mxn: 10293, final_budget_usd: 598.08, sku_order: 3 }
 ];
 
 const POSTMORTEM_DEALS = [
