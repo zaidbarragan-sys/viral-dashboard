17d9d2d29e1a9b4da909f41e3149f5bd30ef865e
Zaid Barragan <zaid@ip-192-168-0-59.ec2.internal>
2026-05-14 17:42:02 -0600

    Pre-viral: VIRAL_DEAL_MODELO_050626 — 3 SKUs, 56,000 MXN

--- data.js
+++ data.js
@@ -2259,9 +2259,31 @@ const PREVIRAL_DEALS = [
     financiamiento_maker: "OC",
     pct_growth_vs_maker: 30,
     categoria_display: "Isotónicos"
+  },
+  {
+    deal_id: "VIRAL_DEAL_MODELO_050626",
+    maker_name: "Grupo Modelo-Ab Inbev",
+    maker_aux: "Modelo",
+    business_unit: "Drinks",
+    descuento_pct: 60,
+    incrementalidad: 6,
+    tipo_viral: "multi-día",
+    ciudades: "Nacional",
+    fecha_planeacion: "2026-05-14",
+    fecha_ejecucion: "2026-06-05",
+    fecha_fin: "2026-06-06",
+    usd_mxn: 17.23,
+    fecha_tasa: "2026-05-14",
+    budget_maker_mxn: 120000,
+    budget_growth_mxn: 36000,
+    budget_total_mxn: 156000,
+    budget_maker_usd: 6965.81,
+    budget_growth_usd: 2089.74,
+    budget_total_usd: 9055.55,
+    financiamiento_maker: "ADS",
+    pct_growth_vs_maker: 30,
+    categoria_display: "Cervezas"
   }
-
-
 
 
 
@@ -2709,7 +2731,11 @@ const PREVIRAL_SKU_DETAIL = [
   { deal_id: "VIRAL_DEAL_GEPP_270426", sync: "8939", ean: "36731323002", pid: "2115620178", name: "Gatorade Bebida Rehidratante sabor Uva 1l - Gatorade - 1000 mL", subcategoria: "Isotónicos", h_units: 85.0, h_orders: 63.6, avg_price: 31.88, ex_incr_units: 935, ex_incr_orders: 700, ex_budget_mxn: 11923, ex_budget_usd: 691.59, real_units: 1349, final_budget_mxn: 17202, final_budget_usd: 997.80, sku_order: 2 },
   { deal_id: "VIRAL_DEAL_GEPP_270426", sync: "12111", ean: "7501022015063", pid: "2115518976", name: "Gatorlyte Naranja", subcategoria: "Isotónicos", h_units: 20.6, h_orders: 13.7, avg_price: 27.95, ex_incr_units: 227, ex_incr_orders: 151, ex_budget_mxn: 2538, ex_budget_usd: 147.22, real_units: 374, final_budget_mxn: 4181, final_budget_usd: 242.52, sku_order: 3 },
   { deal_id: "VIRAL_DEAL_GEPP_270426", sync: "12104", ean: "7501022015049", pid: "2115518975", name: "Gatorlyte Fresa Kiwi", subcategoria: "Isotónicos", h_units: 15.9, h_orders: 10.4, avg_price: 23.97, ex_incr_units: 175, ex_incr_orders: 114, ex_budget_mxn: 1678, ex_budget_usd: 97.33, real_units: 336, final_budget_mxn: 3222, final_budget_usd: 186.89, sku_order: 4 },
-  { deal_id: "VIRAL_DEAL_GEPP_270426", sync: "83639", ean: "7501022016084", pid: "2115514574", name: "Gatorade Bebida Moras Freeze 1 L", subcategoria: "Isotónicos", h_units: 14.1, h_orders: 10.9, avg_price: 36.66, ex_incr_units: 155, ex_incr_orders: 120, ex_budget_mxn: 2273, ex_budget_usd: 131.84, real_units: 194, final_budget_mxn: 2845, final_budget_usd: 165.02, sku_order: 5 }
+  { deal_id: "VIRAL_DEAL_GEPP_270426", sync: "83639", ean: "7501022016084", pid: "2115514574", name: "Gatorade Bebida Moras Freeze 1 L", subcategoria: "Isotónicos", h_units: 14.1, h_orders: 10.9, avg_price: 36.66, ex_incr_units: 155, ex_incr_orders: 120, ex_budget_mxn: 2273, ex_budget_usd: 131.84, real_units: 194, final_budget_mxn: 2845, final_budget_usd: 165.02, sku_order: 5 },
+  // Modelo
+  { deal_id: "VIRAL_DEAL_MODELO_050626", sync: "13692", ean: "7501064196263", pid: "2115901721", name: "Pack Cerveza Clara Modelo Especial 6 x 355 mL", subcategoria: "Cervezas", h_units: 376.5, h_orders: 260.6, avg_price: 147.69, ex_incr_units: 2259, ex_incr_orders: 1564, ex_budget_mxn: 200179, ex_budget_usd: 11620.07, real_units: 1458, final_budget_mxn: 129199, final_budget_usd: 7499.80, sku_order: 1 },
+  { deal_id: "VIRAL_DEAL_MODELO_050626", sync: "79834", ean: "220000093859", pid: "2114998282", name: "Pack Cerveza Corona Clara Lata 4 x 473 mL", subcategoria: "Cervezas", h_units: 44.3, h_orders: 23.7, avg_price: 93.64, ex_incr_units: 266, ex_incr_orders: 142, ex_budget_mxn: 14945, ex_budget_usd: 867.53, real_units: 271, final_budget_mxn: 15226, final_budget_usd: 883.85, sku_order: 2 },
+  { deal_id: "VIRAL_DEAL_MODELO_050626", sync: "79836", ean: "220000094504", pid: "2114998286", name: "Cerveza Michelob Ultra Light 6 x 355 mL", subcategoria: "Cervezas", h_units: 33.6, h_orders: 22.2, avg_price: 120.51, ex_incr_units: 202, ex_incr_orders: 133, ex_budget_mxn: 14606, ex_budget_usd: 847.86, real_units: 160, final_budget_mxn: 11569, final_budget_usd: 671.56, sku_order: 3 }
 
 
 
